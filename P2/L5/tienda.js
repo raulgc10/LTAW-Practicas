35cd623125abe1af75967f3fbcbc99cc3a64f5ac
raulgc10 <raulgomezcantador@gmail.com>
2023-05-01 11:35:17 +0200

    cambios en el server

--- P2/L5/tienda.js
+++ P2/L5/tienda.js
@@ -4,49 +4,30 @@ const path = require('path');
 
 const port = 9000;
 
-//-- Npmbre del fichero JSON a leer
-const USERS = "users.json"
-
-const products_OUT = "products-modificacion.json"
-
-const PRODUCTS = "products.json"
-
-const order = "pedidos.json"
-
+//-- Nombre del fichero JSON a leer
+const SHOP = "tienda.json"
 //-- Leer el fichero JSON
-const  users_json = fs.readFileSync(USERS);
-const  PRODUCTS_JSON = fs.readFileSync(PRODUCTS);
-const  PEDIDOS_JSON = fs.readFileSync(order);
-//-- Crear la estructura tienda a partir del contenido del fichero
-const usuarios = JSON.parse(users_json);
-const productos = JSON.parse(PRODUCTS_JSON);
-const pedidos = JSON.parse(PEDIDOS_JSON);
+const  SHOP_json = fs.readFileSync(SHOP);
 
-pedidos[1]["stock"] = "11"
+//-- Crear la estructura tienda a partir del contenido del fichero
+const TIENDA = JSON.parse(SHOP_json);
 
 //-- Mostrar informacion sobre la tienda
-console.log("Productos en la tienda: " + productos.length);
+console.log("Productos en la tienda: " + TIENDA.products.length);
 //-- Usuarios
-console.log("Usuarios registrados: " + usuarios.length);
+console.log("Usuarios registrados: " + TIENDA.users.length);
 //-- Pedidos pendientes
-console.log("Pedidos pendientes: " + pedidos.length);
+console.log("Pedidos pendientes: " + TIENDA.orders.length);
 
 
 //-- Recorrer el array de productos
-productos.forEach((element, index)=>{
+TIENDA.products.forEach((element, index)=>{
   console.log("Producto: " + (index + 1) + ": " + element["name"]);
 });
 //-- Recorrer el array de usuarios
-usuarios.forEach((element, index)=>{
+TIENDA.users.forEach((element, index)=>{
     console.log("Nombre: " + (index + 1) + ": " + element["username"]);
 });
-pedidos.forEach((element, index)=>{
+TIENDA.orders.forEach((element, index)=>{
     console.log("Pedido: " + (index + 1) + ": " + element["idpedido"]);
 });
-  
-let myJSON = JSON.stringify(pedidos);
-
-//-- Guardarla en el fichero destino
-fs.writeFileSync(products_OUT, myJSON);
-
-console.log("Información guardada en fichero: " + products_OUT);
