3371db8e0502b7a47f5d1238980d3d48a98a0726
raulgc10 <raulgomezcantador@gmail.com>
2023-05-27 21:24:27 +0200

    ya es funcional

--- P4/S10/Version-12/index.js
+++ P4/S10/Version-12/index.js
@@ -1,7 +1,5 @@
 const electron = require('electron');
-
 console.log("Hola desde el proceso de la web...");
-
 //-- Obtener elementos de la interfaz
 const btn_test = document.getElementById("btn_test");
 const display = document.getElementById("display");
@@ -9,25 +7,31 @@ const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
 const print = document.getElementById("print");
+const users = document.getElementById("users");
 
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
 //-- el proceso princpal
-info1.textContent = process.arch;
-info2.textContent = process.platform;
-info3.textContent = process.cwd();
+info1.textContent = process.version;
+info2.textContent = process.versions.electron;
+info3.textContent = process.versions.chrome;
 
+users.textContent = 0;
 
 btn_test.onclick = () => {
-    display.innerHTML += "TEST! ";
+    display.innerHTML += "He pulsado el botón" + "</br>";
     console.log("Botón apretado!");
-
     //-- Enviar mensaje al proceso principal
     electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Boton apretado");
 }
 
 //-- Mensaje recibido del proceso MAIN
-electron.ipcRenderer.on('print', (event, message) => {
+
+  electron.ipcRenderer.on('msg', (event, message) => {
     console.log("Recibido: " + message);
-    print.textContent = message;
+    display.innerHTML += message + "</br>";
+  });
+
+  electron.ipcRenderer.on('users', (event, message) => {
+    users.textContent = message;
   });
--- P4/S10/Version-12/main.js
+++ P4/S10/Version-12/main.js
@@ -51,5 +51,81 @@ electron.ipcMain.handle('test', (event, msg) => {
   console.log("-> Mensaje: " + msg);
 });
 
+//-- Cargar las dependencias
+const socket = require('socket.io');
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+const fs = require('fs');
+
+const PUERTO = 9000;
+
+let date = new Date()
+//-- Crear una nueva aplciacion web
+const app = express();
+
+//-- Crear un servidor, asosiaco a la App de express
+const server = http.Server(app);
+
+//-- Crear el servidor de websockets, asociado al servidor http
+const io = socket(server);
+
+const chat = fs.readFileSync('chat.html','utf-8')
+
+//-- Inicializamos la variable de users
+let users = 0;
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
+//-- Definir el punto de entrada principal de mi aplicación web
+app.get('/', (req, res) => {
+  res.send(chat);
+});
+
+//-- Esto es necesario para que el servidor le envíe al cliente la
+//-- biblioteca socket.io para el cliente
+app.use('/', express.static(__dirname +'/'));
+
+//-- El directorio publico contiene ficheros estáticos
+app.use(express.static('public'));
+
+//------------------- GESTION SOCKETS IO
+//-- Evento: Nueva conexion recibida
+io.on('connect', (socket) => {
+  users = users + 1;
+  console.log('** NUEVA CONEXIÓN **'.green);
+  socket.send("SERVER: Bienvenido al chat")
+  win.webContents.send("users", users);
+  io.send("SERVER: Se ha conectado un nuevo usuario");
+  //-- Evento de desconexión
+  socket.on('disconnect', function(){
+    console.log('** CONEXIÓN TERMINADA **'.red);
+    users = users - 1;
+    win.webContents.send("users", users);
+  });  
+
+  //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
+  socket.on("message", (msg)=> {
+    console.log("Mensaje Recibido!: " + msg.blue);
+    if (msg == "/help"){
+        socket.send("SERVER: /help: Mostrará una lista con todos los comandos soportados <br> /list: Devolverá el número de usuarios conectados <br> /hello: El servidor nos devolverá el saludo <br> /date: Nos devolverá la fecha");
+      } else if (msg == "/list"){
+        socket.send("SERVER: Usuarios conectados:" + users);
+    } else if (msg == "/hello"){
+        socket.send("SERVER: Hola!");
+    } else if (msg == "/date"){
+        socket.send("SERVER: " + date.toLocaleDateString("es-ES"));
+    } else {
+        //-- Reenviarlo a todos los clientes conectados
+        io.send(msg);
+        win.webContents.send("msg", msg);
+    }
+    
+  });
+
+});
+
+//-- Lanzar el servidor HTTP
+//-- ¡Que empiecen los juegos de los WebSockets!
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
 
 
